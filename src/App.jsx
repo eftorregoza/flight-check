d1b886da78c70a549cd38d06ce8883da42816187
Exxel <exxeltorregoza@gmail.com>
2024-09-07 00:47:08 +0800

    Initial setup - set colour scheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+
+  const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem('darkMode')) ?? window.matchMedia('(prefers-color-scheme: dark)').matches)
+
+  function handleDarkMode() {
+    setDarkMode(prev => {
+      localStorage.setItem('darkMode', !prev)
+      return !prev
+    })
+  }
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [darkMode])
 
   return (
-    <>
+    <div className='flex gap-5 flex-col'>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio id earum, consectetur excepturi provident qui commodi natus nisi dignissimos. Illo earum est culpa iure facere error, laborum id eius voluptatibus.</p>
+      <div className="w-11 h-5 bg-orange-500 "></div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button className='mt-5 p-2 border-2 border-slate-800' onClick={handleDarkMode}>Toggle Mode</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
-export default App
